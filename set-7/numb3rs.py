901e78e6a542958d1d288b2303ec628a76337ca8
mike-mcgrath <mike_mcgrath@outlook.com>
2024-05-02 08:52:19 +0300

    Update numb3rs.py

--- set-7/numb3rs.py
+++ set-7/numb3rs.py
@@ -5,17 +5,20 @@
 
 def main() :
 
+    # Get IP address input.
     print( validate( input( 'IPv4 Address: ' ).strip() ) )
  
 def validate( adr ) :
 
     try :
+        # Separate address components.
         adr = adr.split( '.' )
         if len( adr ) != 4 : return False
         
         # To fail test, uncomment this statement...
         # if len( adr ) != 3 : return False
-        
+
+        # Ensure each component is between 0-255.
         for num in adr :
             if int( num ) not in range( 0, 256 ) : return False
         return True
